9d6dd679e7a3c8dc5dc7be4499dca0154f53813d
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-06-19 18:12:01 -0300

    Requisito 3 e 4 Finalizados.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,26 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in instance._data:
+        return None
+
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    print(str(data))
+    instance.enqueue(path_file)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+
+    file = instance.dequeue()
+    print(str(f"Arquivo {file} removido com sucesso"))
 
 
 def file_metadata(instance, position):
