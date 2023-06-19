3aaec6b27081a301b490abce16f327b286d75c4b
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-06-19 20:17:23 -0300

    Requisito 7 Finalizado.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    modified_word = word.lower()
+    result = []
+
+    for file_data in instance._data:
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+        counter = []
+
+        for index, line in enumerate(lines, start=1):
+            if modified_word in line.lower():
+                counter.append({"linha": index})
+
+        if counter:
+            result.append({
+                "palavra": modified_word,
+                "arquivo": file_name,
+                "ocorrencias": counter,
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
