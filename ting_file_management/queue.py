c84766909ce6fe160cb60365a31809cf8c56cd2e
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-06-19 16:57:11 -0300

    Início do Projeto

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,10 +3,10 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
